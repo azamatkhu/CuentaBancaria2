59309d115021b4e95330b5c69b405995930c0546
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:17 +0200

    Bulk upload

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class CuentaBancaria {
+    private double saldo;
+
+    public CuentaBancaria(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    // Si el saldo es insuficiente, tiramos una error SaldoInsuficienteException
+    public void Retirar(double dinero) throws SaldoInsuficienteException {
+        {
+            if (dinero > saldo) {
+                throw new SaldoInsuficienteException("Saldo insuficiente!");
+            }
+
+            this.saldo -= dinero;
+
+            System.out.println("Has retirado " + dinero);
+            System.out.println("-------------------------------");
+            System.out.println("Nuevo Saldo: " + this.saldo);
+        }
+    }
 }
--- src/SaldoInsuficienteException.java
+++ src/SaldoInsuficienteException.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class SaldoInsuficienteException {
+// Esta clase utilizamos para crear un error personalizado, utilizando extends Exception
+public class SaldoInsuficienteException extends Exception {
+    public SaldoInsuficienteException(String mensaje) {
+        super(mensaje);
+    }
 }
